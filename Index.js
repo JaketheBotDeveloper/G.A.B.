1b65a3a17f36ccd7935711ba8c5924c319eb5653
Jake18122003 <jake@Jakes-MacBook-Air.local>
2018-12-02 16:21:07 +0000

    Still trying

--- Index.js
+++ Index.js
@@ -3,8 +3,10 @@ const Commando = require('discord.js-commando');
 const bot = new Commando.Client({
     owner: '344217669450596363'
 });
-//Need to add Jake soon.
-//Need to add a || to add Jake
+/*
+Need to add Jake soon.
+Need to add a || to add Jake
+*/
 bot.registry.registerGroup('simple', 'Simple');
 bot.registry.registerGroup('music', 'Music');
 bot.registry.registerGroup('admin', 'Admin');
