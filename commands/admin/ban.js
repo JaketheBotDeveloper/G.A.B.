3cc870520942fdc2f1b03c17a724dc4dcf1b1a53
eazoppe <45536606+eazoppe@users.noreply.github.com>
2018-12-07 22:06:22 -0500

    stuff

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -13,32 +13,8 @@ class BanCommand extends commando.Command
   }
 
   async run(message, args)
-  {if (!message.guild) return;
-  if (message.content.startsWith('!ban')) {
-    const user = message.mentions.users.first();
-    if (user) {
-      {if (!message.member.hasPermission("BAN_MEMBERS"));
-      const member = message.guild.member(user);
-      if (member) {
-        member.ban({
-          reason: 'They were bad!',
-        }).then(() => {
-          message.reply(`Successfully banned ${user.tag}`);
-        }).catch(err => {
-          message.reply('I was unable to ban the member');
-          console.error(err);
-        });
-      } else {
-        message.reply('That user isn\'t in this guild!');
-      }
-    }}
-    else 
-    {
-      message.reply("You don't have that permission");
-    }
-    } else {
-      message.reply('You didn\'t mention the user to ban or you dont have permission');
-    }
+  {
+    message.reply("This is being worked on.")
   }
 };
 
--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -14,55 +14,7 @@ class KickCommand extends commando.Command
 
     async run(message, args)
     {
-
-      let cantfindchannel = new Discord.RichEmbed()
-      .setDescription("Error")
-      .setColor("#00ff3f")
-      .addField("You got this error because: ", "there is No #incidents Channel please make one.")
-      .setTimestamp();
-
-      let nopermision = new Discord.RichEmbed()
-      .setDescription("Error")
-      .setColor("#00ff3f")
-      .addField("You got this error because: ", "You don't have permision to kick users.")
-      .setTimestamp();
-
-      let Userhaspermision = new Discord.RichEmbed()
-      .setDescription("Error")
-      .setColor("#00ff3f")
-      .addField("You got this error because: ", "That user has permisions")
-      .setTimestamp();
-
-      let cantkick = new Discord.RichEmbed()
-      .setDescription("Error")
-      .setColor("#00ff3f")
-      .addField("You got this error because: ", "The Bot does not have permision to kick")
-      .setTimestamp();
-
-      let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!kUser) return message.channel.send("Can't find that user!")
-      let kReason = args.slice(1).join(" ");
-      if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(nopermision);
-      if(kUser.hasPermision("KICK_MEMBERS") || kUser.hasPermision("BAN_MEMBERS")) return message.channel.send(Userhaspermision);
-    let messageuser = message.author.tag;
-      let kickEmbed = new Discord.RichEmbed()
-      .setDescription("~Kick~")
-      .setColor("#00ff3f")
-      .addField("Kicked User", `${kUser} with ID: ${kUser.id}`)
-      .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-      .addField("Kicked In", message.channel)
-      .addField("Time", message.createdAt)
-      .addField("Reason", kReason)
-
-      let kickChannel = message.guild.channels.find(`name`, "incidents");
-      if(!kickChannel) return message.channel.send(cantfindchannel);
-    try {
-      message.guild.member(kUser).kick(kReason + ` Kicked by: ${messageuser}`);
-      kickChannel.send(kickEmbed);
-    } catch(e) {
-      message.channel.send(cantkick);
+      message.reply("This is being worked on")
     }
-          };
-    }
-
+  }
 module.exports = KickCommand;
