c5f92e6c7dcb17e5d34729cd59c55264c575d738
eazoppe <45536606+eazoppe@users.noreply.github.com>
2018-12-15 17:26:47 -0500

    Whatyoudidinhere

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -16,7 +16,7 @@ class InviteCommand extends commando.Command
     {
         let embed = new Discord.RichEmbed()
         .setTitle("Invite")
-        .setDescription("Invite Link:" + "Link: https://discordapp.com/oauth2/authorize?client_id=495593444513677320&scope=bot&permissions=2146958847")
+        .setDescription("Invite Link:" + "Link: https://discordapp.com/oauth2/authorize?client_id=473231079755612160&scope=bot&permissions=2146958847")
         .addField("DBL Link: ", "https://discordbots.org/bot/473231079755612160")
         .setTimestamp();
         let dm = message.author;
