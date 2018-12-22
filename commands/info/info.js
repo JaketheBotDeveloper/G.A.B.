eb88d110529085f8cde7ec4810f4d264ca2153a0
eazoppe <45536606+eazoppe@users.noreply.github.com>
2018-12-21 20:18:59 -0500

    Stuff

--- commands/info/info.js
+++ commands/info/info.js
@@ -18,6 +18,7 @@ class InfoCommand extends commando.Command
         .setTitle("Info about Sith Bot")
         .setDescription("I am G.A.B.. G.A.B. stands for Gaming Administration Bot. I am always being worked on to be improved and get me better. If you want to invite me you can do !invite to invite me. I can kick people and ban people.")
         .addField("I am made by eazoppe#9471 and Jake18122003#5483. If you need any help or need to report a bug you can join the G.A.B. Support Server. Here is the link: https://discord.gg/CaX7gX8")
+        .setThumbnail('https://cdn.discordapp.com/attachments/518557138281562135/525843370636083220/GAB.png')
         .setColor(0xf38b03);
         let dm = message.author;
         try {
--- commands/info/invite.js
+++ commands/info/invite.js
@@ -17,9 +17,10 @@ class InviteCommand extends commando.Command
         let embed = new Discord.RichEmbed()
         .setTitle("Invite")
         .setDescription("Invite Link:" + "Link: https://discordapp.com/oauth2/authorize?client_id=473231079755612160&scope=bot&permissions=2146958847")
-        .addField("DBL Link: ", "https://discordbots.org/bot/473231079755612160")
-        .setFooter("G.A.B. Support Server: https://discord.gg/CaX7gX8")
+        .addField("DBL Link: ", "https://discordbots.org/bot/473231079755612160 | G.A.B. Support Server: https://discord.gg/CaX7gX8")
+        .setFootr("Thank you for using G.A.B.")
         .setThumbnail('https://cdn.discordapp.com/attachments/518557138281562135/525843370636083220/GAB.png')
+        .setColor(0x39ff65)
         .setTimestamp();
         let dm = message.author;
         try {
